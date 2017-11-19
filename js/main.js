291132594f3384b82c7bfbe8da19db82761cb697
sander102907 <s.d.bruin@hotmail.com>
2017-11-19 14:36:00 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ $(document).ready(() => {
   $('.information').on('click', event => {
       $(event.currentTarget).closest('.item').find('.extra-info').slideDown();
       $(event.currentTarget).find('.more-info').toggleClass('rotate');
+      return false;
   });
 
   $('.item').on('mouseleave', event => {
     $(event.currentTarget).find('.extra-info').slideUp();
     $(event.currentTarget).find('.more-info').toggleClass('rotate-reset');
+    return false;
   });
 
 })
